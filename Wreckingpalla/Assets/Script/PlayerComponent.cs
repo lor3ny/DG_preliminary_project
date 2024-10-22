d888f6eebf7663aaa6e9c55302055a7ce1138776
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2024-10-22 16:53:36 +0100

    Movement, Collisions and Cinemachine

--- Wreckingpalla/Assets/Script/PlayerComponent.cs
+++ Wreckingpalla/Assets/Script/PlayerComponent.cs
@@ -1,32 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerComponent : MonoBehaviour
 {
     public float speed = 0;
 
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private Vector3 inputVector;
+    int up = 0, right = 0, down = 0, left = 0;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    void onMove(InputValue movementValue)
+    private void Update()
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        //int ciao = Input.GetKeyDown(KeyCode.W);
+
+        if (Input.GetKeyDown(KeyCode.W))
+            up = 1;
+
+        if (Input.GetKeyDown(KeyCode.D))
+            right = 1;
+
+        if (Input.GetKeyDown(KeyCode.S))
+            down = -1;
+
+        if (Input.GetKeyDown(KeyCode.A))
+            left = -1;
+
+        if (Input.GetKeyUp(KeyCode.W))
+            up = 0;
+
+        if (Input.GetKeyUp(KeyCode.D))
+            right = 0;
+
+        if (Input.GetKeyUp(KeyCode.S))
+            down = 0;
+
+        if (Input.GetKeyUp(KeyCode.A))
+            left = 0;
+
+
+        inputVector = new Vector3(left+right, 0.0f, up+down);
+        //Debug.Log(inputVector);
     }
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(inputVector * speed);
+    }
 
-        rb.AddForce(movement * speed);
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            Debug.Log("hit");
+            Destroy(other.gameObject);
+        }
     }
 }

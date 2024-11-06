761ff9d61f9fc74d16f3b257f0c75bfeb3537123
noemimessori <noemimessori1@gmail.com>
2024-11-06 13:24:04 +0100

    models. some fixes

---
+++ Wreckingpalla/Assets/Script/SettingMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
